bb40afc585b6ed6adb3be863b3a5d9a3a50910ca
nicusorpetcu <petcu.nicusor@gmail.com>
2025-06-04 21:39:48 +0300

    initial commit

---
+++ src/main/java/com/example/store/StoreManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagementToolApplication.class, args);
+    }
+
+}
